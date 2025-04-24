895cc8aec52da6d9b473a9125a2b16502b1dbf81
Clo-jo <chloe.swan@landregistry.gov.uk>
2025-04-24 14:13:53 +0100

    added phase banner and replaced header

--- app/routes.js
+++ app/routes.js
@@ -9,7 +9,11 @@ const router = govukPrototypeKit.requests.setupRouter()
 // Add your routes here
 
 router.post('/new/email-address', (req,res) => {
+    if (req.query.returnUrl){
+        res.redirect(req.query.returnUrl)
+        } else {
     res.redirect ('/new/code')
+        }
 })
 
 router.post('/new/code', (req,res) => {
@@ -17,15 +21,22 @@ router.post('/new/code', (req,res) => {
 })
 
 router.post('/new/name', (req,res) => {
+    if (req.query.returnUrl){
+    res.redirect(req.query.returnUrl)
+    } else {
     res.redirect ('/new/country')
+    }
 })
 
 router.post('/new/country', (req,res) => {
+    if (req.query.returnUrl){
+    res.redirect(req.query.returnUrl)
+    } else {
     res.redirect ('/new/juggling-balls')
+    }
 })
 
 router.post('/new/juggling-balls', (req,res) => {
-
 
     if (req.body.new.numberOfBalls == "None - I cannot juggle"){
     res.redirect ('/new/not-eligible')
@@ -35,9 +46,22 @@ router.post('/new/juggling-balls', (req,res) => {
 })
 
 router.post('/new/juggling-trick', (req,res) => {
+    if (req.query.returnUrl){
+        res.redirect(req.query.returnUrl)
+        } else {
     res.redirect ('/new/juggling-objects')
+        }
 })
 
 router.post('/new/juggling-objects', (req,res) => {
+    if (req.query.returnUrl){
+        res.redirect(req.query.returnUrl)
+        } else {
     res.redirect ('/new/check')
+        }
+})
+
+
+router.post('/new/check', (req,res) => {
+    res.redirect ('/new/confirmation')
 })
